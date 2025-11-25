08e2f36accb52aada812d56c9008a52269b31701
vbounmy <valentine.bounmy@gmail.com>
2025-11-25 23:43:59 +0100

    landing page changes

--- app.py
+++ app.py
@@ -169,33 +169,6 @@ def decide_final_title_llm(lyrics_fragment, genius_results, google_results):
         title, artist = "Aucune correspondance trouvée", "Unknown"
     return {"title": title, "artist": artist}
 
-
-# ---------------------------
-# YOUTUBE LINK TO EMBED
-# ---------------------------
-
-def youtube_to_embed(url: str):
-    if url is None:
-        return None
-
-    # 1st case : standard youtube links
-    if "watch?v=" in url:
-        video_id = url.split("watch?v=")[1].split("&")[0]
-        return f"https://www.youtube.com/embed/{video_id}"
-
-    # 2nd case : liens m.youtube.com links
-    if "v=" in url:
-        video_id = url.split("v=")[1].split("&")[0]
-        return f"https://www.youtube.com/embed/{video_id}"
-
-    # 3rd case : shorts links
-    if "/shorts/" in url:
-        video_id = url.split("/shorts/")[1].split("?")[0]
-        return f"https://www.youtube.com/embed/{video_id}"
-
-    return None
-
-
 # ---------------------------
 # PIPELINE
 # ---------------------------
@@ -240,16 +213,8 @@ def vocal_pipeline(audio, language):
         final_song = decide_final_title_llm(lyrics_fragment, genius_results, google_results)
         final_title = f"{final_song['title']} by {final_song['artist']}"
         youtube_md = f"[Watch on YouTube]({youtube_link})" if youtube_link else "No YouTube link found"
-        embed_url = youtube_to_embed(youtube_link)
-        iframe_html = (
-            f'<iframe width="900" height="500" src="{embed_url}" '
-            f'frameborder="0" allow="accelerometer; autoplay; clipboard-write; '
-            f'encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            if embed_url
-            else "<p>No video available</p>"
-        )
 
-        return text, lyrics_fragment, genius_results, google_results, final_title, youtube_md, iframe_html
+        return text, lyrics_fragment, genius_results, google_results, final_title, youtube_md
     
     except Exception as e:
         import traceback
@@ -265,10 +230,13 @@ def vocal_pipeline(audio, language):
 def img_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode("utf-8")
-    
-logo_base64 = img_to_base64("images/logo_vinyle_cover.png")
-vinyle_base64 = img_to_base64("images/vinyle.png")
 
+# Images in base64
+logo_vinyle_base64 = img_to_base64("images/logo_vinyle_cover.png")
+vinyle_base64 = img_to_base64("images/vinyle.png")
+logo_base64 = img_to_base64("images/logo.png")
+
+# Custom CSS
 custom_css = """
 * {
     font-family: "Inter", sans-serif !important;
@@ -281,6 +249,7 @@ body {
     background: transparent !important;
     color: white !important;
 }
+
 label{
     background: transparent !important;
     color: white !important;
@@ -296,7 +265,7 @@ textarea, input, select {
 @keyframes fadeInText {
     from { 
         opacity: 0;
-        transform: translateX(-50px);
+        transform: translateX(220px);
     }
     to { 
         opacity: 1;
@@ -305,17 +274,11 @@ textarea, input, select {
 }
 
 @keyframes vinyleEntry {
-    0% { 
-        opacity: 0; 
-        transform: translate(-50%, -50%) scale(0.5);
+    0% {
+        transform: translate(-60%, -50%);
     }
-    50% { 
-        opacity: 1; 
-        transform: translate(-50%, -50%) scale(1);
-    }
-    100% { 
-        opacity: 1; 
-        transform: translate(180%, -50%) scale(1);
+    100% {
+        transform: translate(3%, -50%);
     }
 }
 @keyframes spin {
@@ -323,33 +286,212 @@ textarea, input, select {
     to   { transform: rotate(360deg); }
 }
 
+@keyframes aurora-1 {
+  0% {
+    top: 0;
+    right: 0;
+  }
+
+  50% {
+    top: 100%;
+    right: 75%;
+  }
+
+  75% {
+    top: 100%;
+    right: 25%;
+  }
+
+  100% {
+    top: 0;
+    right: 0;
+  }
+}
+
+@keyframes aurora-2 {
+  0% {
+    top: -50%;
+    left: 0%;
+  }
+
+  60% {
+    top: 100%;
+    left: 75%;
+  }
+
+  85% {
+    top: 100%;
+    left: 25%;
+  }
+
+  100% {
+    top: -50%;
+    left: 0%;
+  }
+}
+
+@keyframes aurora-3 {
+  0% {
+    bottom: 0;
+    left: 0;
+  }
+
+  40% {
+    bottom: 100%;
+    left: 75%;
+  }
+
+  65% {
+    bottom: 40%;
+    left: 50%;
+  }
+
+  100% {
+    bottom: 0;
+    left: 0;
+  }
+}
+
+@keyframes aurora-4 {
+  0% {
+    bottom: -50%;
+    right: 0;
+  }
+
+  50% {
+    bottom: 0%;
+    right: 40%;
+  }
+
+  90% {
+    bottom: 50%;
+    right: 25%;
+  }
+
+  100% {
+    bottom: -50%;
+    right: 0;
+  }
+}
+
+@keyframes aurora-border {
+  0% {
+    border-radius: 37% 29% 27% 27% / 28% 25% 41% 37%;
+  }
+
+  25% {
+    border-radius: 47% 29% 39% 49% / 61% 19% 66% 26%;
+  }
+
+  50% {
+    border-radius: 57% 23% 47% 72% / 63% 17% 66% 33%;
+  }
+
+  75% {
+    border-radius: 28% 49% 29% 100% / 93% 20% 64% 25%;
+  }
+
+  100% {
+    border-radius: 37% 29% 27% 27% / 28% 25% 41% 37%;
+  }
+}
+
 #landing{
     margin: auto;
-    padding: 50px;
+    padding: 20px 50px;
     max-width: 820px;
     display: flex;
     flex-direction: column;
-    gap: 30px;
+    gap: 10px;
     justify-content: center;
     align-items: center;
-    background: rgba(0,0,0,0.45);
+    background: rgba(0,0,0,1);
     border: 1px solid rgba(79,70,229,0.9);
     border-radius: 26px;
-    box-shadow: 0 0 35px rgba(79,70,229,0.5);
+    box-shadow: 0 0 200px rgba(79,70,229,0.5);
     backdrop-filter: blur(6px);
 }
-#landing-welcome{
-    font-size: 20px !important;
-    height: 50px;
+
+.gr-html, 
+.gr-html > .wrap {
+    isolation: unset !important;
+    background: transparent !important;
 }
+.aurora-container {
+    position: relative;
+    width: 100%;
+    text-align: center;
+    padding: 20px 0 10px 0;
+    overflow: hidden;
+    display: inline-block;
+}
+.aurora-title {
+    font-size: 48px !important;
+    font-weight: 900;
+    letter-spacing: clamp(-1.75px, -0.25vw, -3.5px);
+    position: relative;
+    overflow: hidden;
+    background: rgba(0,0,0,1) !important;
+    margin: 0;
+}
+.aurora {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    mix-blend-mode: darken !important;
+    pointer-events: none;
+}
+.aurora__item {
+    overflow: hidden;
+    position: absolute;
+    width: 60vw;
+    height: 60vw;
+    background-color: #00c2ff;
+    border-radius: 37% 29% 27% 27% / 28% 25% 41% 37%;
+    filter: blur(1rem);
+    mix-blend-mode: overlay;
+}
+
+.aurora__item:nth-of-type(1) {
+  top: -50%;
+  animation: aurora-border 6s ease-in-out infinite,
+    aurora-1 12s ease-in-out infinite alternate;
+}
+.aurora__item:nth-of-type(2) {
+  background-color: #ffc640;
+  right: 0;
+  top: 0;
+  animation: aurora-border 6s ease-in-out infinite,
+    aurora-2 12s ease-in-out infinite alternate;
+}
+.aurora__item:nth-of-type(3) {
+  background-color: #33ff8c;
+  left: 0;
+  bottom: 0;
+  animation: aurora-border 6s ease-in-out infinite,
+    aurora-3 8s ease-in-out infinite alternate;
+}
+.aurora__item:nth-of-type(4) {
+  background-color: #e54cff;
+  right: 0;
+  bottom: -50%;
+  animation: aurora-border 6s ease-in-out infinite,
+    aurora-4 24s ease-in-out infinite alternate;
+}
+
+
 #landing-container {
     position: relative;
-    width: 600px;
+    width: 100%;
     height: 250px;
     margin: auto;
     display: flex;
     align-items: center;
     justify-content: center;
+    gap: 10px;
     overflow: hidden;
 }
 #landing-text {
@@ -357,15 +499,16 @@ textarea, input, select {
     text-align: right;
     font-size: 16px !important;
     position: absolute;
-    left: 0;
+    left: 30px;
     color: white;
+    right: 0;
     opacity: 0;
-    animation: fadeInText 1s forwards 0.5s;
+    animation: fadeInText 2s forwards 0.5s;
 }
 #landing-text hr {
-    border: 1px solid #4F46E5;
-    width: 90px;
-    margin: 10px 0;
+    border: 1px solid #FFFFFF;
+    width: 60px;
+    margin: 10px 0 10px auto;
 }
 #landing-logo,
 #landing-logo .gr-image,
@@ -379,18 +522,21 @@ textarea, input, select {
     border: none !important;
     box-shadow: none !important;
 }
+#landing-vinyle-wrapper {
+    position: absolute;
+    left: 52%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 188px;
+    height: 188px;
+    animation: vinyleEntry 2s ease forwards 0.5s;
+}
 #landing-vinyle {
     width: 188px;
     height: 188px;
     border-radius: 50%;
-    position: absolute;
-    left: 52%;
-    transform: translate(-50%, -50%) scale(0.5);
-    opacity: 0;
-    z-index: 1;
-    animation: vinyleEntry 1.5s ease forwards 0.5s, spin 10s linear infinite;
+    animation: spin 10s linear infinite;
 }
-
 
 .landing-enter-btn {
     display: flex !important;
@@ -399,41 +545,53 @@ textarea, input, select {
 }
 #enter-btn {
     height: 50px !important;
-    width: 180px !important;
+    width: 200px !important;
     font-size: 20px !important;
     font-weight: 300 !important;
     border-radius: 50px !important;
     background: rgb(79,70,229) !important;
     color: white !important;
 }
+#enter-btn:hover {
+    background: transparent !important;
+    color: rgb(255,255,255) !important;
+    border: 1px solid #4F46E5 !important;
+}
 
-.header{
-    margin: auto;
-    padding: 50px;
-    width: auto !important;
-    max-width: fit-content !important;
-    display: flex;
-    flex-direction: row;
-    gap: 15px;
+#main-page{
+    padding: 5px 100px 10px 100px;
     justify-content: center;
     align-items: center;
+}
+
+.header-container{
+    width: 900px;
     background: rgba(0,0,0,0.45);
     border: 1px solid rgba(79,70,229,0.9);
-    border-radius: 26px;
+    border-radius: 50px;
     box-shadow: 0 0 35px rgba(79,70,229,0.5);
     backdrop-filter: blur(6px);
 }
-#app-logo,
-#app-logo .gr-image,
-#app-logo .wrap,
-#app-logo .container {
-    background: transparent !important;
-    border: none !important;
-    box-shadow: none !important;
+
+.header{
+    height: 70px;
+    width: 900px;
+    padding: 0px 20px;
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+    justify-content: flex-start;
+    align-items: center;
 }
-#app-logo img {
-    width: 64px;
-    height: 64px;
+#app-logo {
+    width: 80px;
+    height: 80px;
+    background: transparent !important;
+}
+#header-text {
+    max-width: 500px;
+    color: white;
+    font-size: 16px !important;
 }
 
 .neon-box {
@@ -455,20 +613,41 @@ textarea, input, select {
     font-weight: 500 !important;
 }
 
-#vinyle,
-#vinyle .gr-image,
-#vinyle .wrap,
-#vinyle .container {
+.final-result-container {
+    margin: 15px;
+    padding: 20px;
     background: transparent !important;
-    border: none !important;
-    box-shadow: none !important;
+    border: 1px solid rgba(79,70,229,0.9) !important;
+    border-radius: 25px !important;
+    box-shadow: 0 0 18px rgba(79,70,229,0.5);
+    backdrop-filter: blur(6px);
+    overflow: hidden;
 }
-#vinyle img {
-    width: 250px;
-    height: 250px;
+
+#vinyle-and-title {
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    gap: 40px !important;
+    overflow: hidden;
+}
+.vinyle-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+}
+#vinyle {
+    width: 200px;
+    height: 200px;
+    overflow: hidden;
     background: transparent !important;
     border-radius: 50%;
     animation: spin 10s linear infinite;
+}
+#final-title-box {
+    max-width: 350px !important;
+    text-align: center !important;
 }
 
 .back_clear_btn{
@@ -481,6 +660,20 @@ textarea, input, select {
     width: auto !important;
     max-width: fit-content !important;
 }
+#back-btn {
+    background: #505050 !important;
+    color: white !important;
+    height: 30px !important;
+    width: 130px !important;
+    font-size: 16px !important;
+    border-radius: 50px !important;
+    font-weight: 500 !important;
+}
+#back-btn:hover {
+    background: transparent !important;
+    color: rgb(255,255,255) !important;
+    border: 1px solid #505050 !important;
+}
 #clear-btn{
     background: #505050 !important;
     color: white !important;
@@ -490,14 +683,10 @@ textarea, input, select {
     border-radius: 50px !important;
     font-weight: 500 !important;
 }
-#back-btn {
-    background: #505050 !important;
-    color: white !important;
-    height: 30px !important;
-    width: 200px !important;
-    font-size: 16px !important;
-    border-radius: 50px !important;
-    font-weight: 500 !important;
+#clear-btn:hover {
+    background: transparent !important;
+    color: rgb(255,255,255) !important;
+    border: 1px solid #505050 !important;
 }
 """
 
@@ -516,11 +705,17 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="violet")) as demo:
 
         with gr.Column(visible=True, elem_classes="landing-welcome-container"):
             gr.HTML("""
-                <div id="landing-welcome">
-                    <div id="landing-text">
-                        <p>Welcome!</p>
+            <div class="aurora-container">
+                <h1 class="aurora-title">
+                    Welcome on <b>SoundFinder</b>
+                    <div class="aurora">
+                        <div class="aurora__item"></div>
+                        <div class="aurora__item"></div>
+                        <div class="aurora__item"></div>
+                        <div class="aurora__item"></div>
                     </div>
-                </div>
+                </h1>
+            </div>
             """)
 
         with gr.Column(visible=True, elem_classes="landing-vinyle-container"):
@@ -531,24 +726,29 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="violet")) as demo:
                         <hr>
                         <p>JUST A <b>CLICK</b> AWAY</p>
                     </div>
-                    <img id="landing-logo" src="data:image/png;base64,{logo_base64}" />
-                    <img id="landing-vinyle" src="data:image/png;base64,{vinyle_base64}" />
+                    <img id="landing-logo" src="data:image/png;base64,{logo_vinyle_base64}" />
+                    <div id="landing-vinyle-wrapper">
+                        <img id="landing-vinyle" src="data:image/png;base64,{vinyle_base64}" />
+                    </div>
                 </div>
             """)
 
         with gr.Column(visible=True, elem_classes="landing-enter-btn"):
-            enter_btn = gr.Button("🎧 Enter App", elem_id="enter-btn")
+            enter_btn = gr.Button("🎧 Enter App →", elem_id="enter-btn")
 
     # ====== APP PAGE ======
-    with gr.Column(visible=False) as app_col:
+    with gr.Column(visible=False, elem_id="main-page") as app_col:
 
-        with gr.Column(elem_classes="header"):
-            gr.Image("images/logo.png", elem_id="app-logo", show_label=False)
-            gr.HTML("""
-                <div id=header-text>
-                    <p>Find a song easily from your voice,</p>
-                    <p>in any language</p>
-                </div>
+        with gr.Row(elem_classes="header-container"):
+            gr.HTML(f"""
+                <div class="header">
+                    <div id="app-logo">
+                        <img src="data:image/png;base64,{logo_base64}" />
+                    </div>
+                    <div id=header-text>
+                        <p>Find a song easily from your voice, in any language</p>
+                    </div>
+                </class>
             """)
 
         with gr.Column(elem_classes="neon-box"):
@@ -566,45 +766,49 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="violet")) as demo:
                 show_label=False
             )
 
-        submit_btn = gr.Button("Submit", elem_id="submit-btn")
+        submit_btn = gr.Button("Search", elem_id="submit-btn")
 
-        with gr.Accordion("Transcription", open=True, elem_classes="neon-box"):
+        with gr.Accordion("Transcription", open=True, elem_classes="neon-box", visible=False):
             out_transcript = gr.Textbox(lines=3, show_label=False)
 
-        with gr.Accordion("Extracted lyrics", open=True, elem_classes="neon-box"):
+        with gr.Accordion("Extracted lyrics", open=True, elem_classes="neon-box", visible=False):
             out_lyrics = gr.Textbox(lines=3, show_label=False)
 
-        with gr.Accordion("Genius results", open=True, elem_classes="neon-box"):
+        with gr.Accordion("Genius results", open=True, elem_classes="neon-box", visible=False):
             out_genius = gr.JSON()
 
-        with gr.Accordion("Google results", open=True, elem_classes="neon-box"):
+        with gr.Accordion("Google results", open=True, elem_classes="neon-box", visible=False):
             out_google = gr.JSON()
 
-        with gr.Column(elem_classes="neon-box"):
+        with gr.Column(elem_classes="final-result-container"):
             gr.HTML("""
             <div id=final-result-text>
                 <p>THE <b>SONG</b> YOU ARE LOOKING FOR</p>
                 <p>seems to be ...</p>
             </div>
             """)
-            gr.Image("images/vinyle.png", elem_id="vinyle", show_label=False)
-            out_final_title = gr.Textbox(lines=2, show_label=False)
+            with gr.Row(elem_id="vinyle-and-title"):
+                gr.HTML(f"""
+                    <div class="vinyle-container">
+                        <img id="vinyle" src="data:image/png;base64,{vinyle_base64}" />
+                    </div>
+                """)
+                out_final_title = gr.Textbox(lines=2, show_label=False, elem_id="final-title-box")
             out_youtube = gr.Markdown(label="YouTubes Link")
-            out_iframe = gr.HTML(show_label=False)
             
         with gr.Column(visible=True, elem_classes="back_clear_btn"):
-          back_btn  = gr.Button("Back to Landing", elem_id="back-btn")
+          back_btn  = gr.Button("Back", elem_id="back-btn")
           clear_btn = gr.Button("Clear", elem_id="clear-btn")
 
         submit_btn.click(
             fn=vocal_pipeline,
             inputs=[audio_input, lang_choice],
-            outputs=[out_transcript, out_lyrics, out_genius, out_google, out_final_title, out_youtube, out_iframe]
+            outputs=[out_transcript, out_lyrics, out_genius, out_google, out_final_title, out_youtube]
         )
 
         clear_btn.click(
-            fn=lambda: ("", "", [], [], "", ""),
-            outputs=[out_transcript, out_lyrics, out_genius, out_google, out_final_title, out_youtube, out_iframe]
+            fn=lambda: ("", "", [], [],"", ""),
+            outputs=[out_transcript, out_lyrics, out_genius, out_google, out_final_title, out_youtube]
         )
 
     # ====== NAVIGATION BUTTONS
